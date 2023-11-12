fe32b8f019dca347e378c64902fd41f6d4c76d9c
Lopfi <lopfi95@gmail.com>
2023-11-12 01:18:01 +0100

    fix card name issues by using regex

--- src/lib/sets.ts
+++ src/lib/sets.ts
@@ -20,37 +20,41 @@ export type Card = {
 async function loadSets() {
   if (sets.length !== 0) return;
   sets = [];
+  const regex = /\/public\/sets\/(.*)\/(.*).(png|jpg|jpeg)/;
   for (const path in images) {
     const actualPath = path.replace("/public", "/guess-whu");
-    const setName = path.split("/")[3] as string;
-    const cardName = path.split("/")[4].split(".")[0];
-    const set = sets.find((set) => set.name === setName);
-    if (set) {
-      if (cardName !== "thumbnail")
-        set.cards.push({
-          name: cardName,
-          image: actualPath,
-        });
-      else {
-        set.thumbnail = actualPath;
-      }
-    } else {
-      if (cardName !== "thumbnail")
-        sets.push({
-          name: setName,
-          cards: [
-            {
-              name: cardName,
-              image: actualPath,
-            },
-          ],
-        });
-      else {
-        sets.push({
-          name: setName,
-          cards: [],
-          thumbnail: actualPath,
-        });
+    const match = path.match(regex);
+    if (match) {
+      const setName = match[1];
+      const cardName = match[2];
+      const set = sets.find((set) => set.name === setName);
+      if (set) {
+        if (cardName !== "thumbnail")
+          set.cards.push({
+            name: cardName,
+            image: actualPath,
+          });
+        else {
+          set.thumbnail = actualPath;
+        }
+      } else {
+        if (cardName !== "thumbnail")
+          sets.push({
+            name: setName,
+            cards: [
+              {
+                name: cardName,
+                image: actualPath,
+              },
+            ],
+          });
+        else {
+          sets.push({
+            name: setName,
+            cards: [],
+            thumbnail: actualPath,
+          });
+        }
       }
     }
   }
